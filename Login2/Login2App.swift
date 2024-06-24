1085d42daa13fd68353674a81db37855162b9b24
Aref <devfera14@gmail.com>
2024-06-24 17:21:05 -0300

    Initial Commit

---
+++ Login2/Login2App.swift
@@ -0,0 +1,17 @@
+//
+//  Login2App.swift
+//  Login2
+//
+//  Created by Aref Chucri on 24/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Login2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
